930174665c48f4944ff74113325f1065bc1735dc
RoliesDonald <rolies.donald@gmail.com>
2020-06-12 01:38:02 +0700

    Big init

---
+++ backend/views/layouts/_sidebar.php
@@ -0,0 +1,24 @@
+<?php
+
+?>
+
+<aside>
+    <?php echo \yii\bootstrap4\Nav::widget([
+        'options' => [
+            'class' => 'd-flex flex-column nav-pills'
+        ],
+        'items' => [
+            [
+                'label' => 'Dashboard',
+                'url' => ['/site/index']
+            ],
+            [
+                'label' => 'Videos',
+                'url' => ['/video/index']
+            ]
+        ]
+    ])
+
+    ?>
+
+</aside>
